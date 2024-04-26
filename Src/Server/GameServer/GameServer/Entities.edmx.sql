e2599b2b3080617eb2c9d763511f9560af58f921
zhangyujian111 <142076498+zhangyujian111@users.noreply.github.com>
2024-04-26 21:28:39 +0800

    初始化成功

--- Src/Server/GameServer/GameServer/Entities.edmx.sql
+++ Src/Server/GameServer/GameServer/Entities.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 09/07/2018 00:02:53
--- Generated from EDMX file: D:\Projects\ExtremeWorld\project-z\Src\Server\GameServer\GameServer\Entities.edmx
+-- Date Created: 04/26/2024 21:23:14
+-- Generated from EDMX file: D:\Projects\MyMMORPG\Src\Server\GameServer\GameServer\Entities.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
